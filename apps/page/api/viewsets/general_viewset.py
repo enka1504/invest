e0fbca4cbf9c06431fcdf947e59c0f1045ea1211
enka1504 <ramos_rafe@hotmail.com>
2023-08-07 13:39:30 -0600

    cambiando lisapiview

--- apps/page/api/viewsets/general_viewset.py
+++ apps/page/api/viewsets/general_viewset.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.generics import ListAPIView
 from apps.page.models import Banner, Landing_Page
 from apps.page.api.serializers.general_serializer import BannerSerializer, LandingSerializer
 
-class BannerViewSet(viewsets.ModelViewSet):
+class BannerViewSet(ListAPIView):
     queryset = Banner.objects.all()
     serializer_class = BannerSerializer
 
