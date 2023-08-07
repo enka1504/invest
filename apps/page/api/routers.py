ae19eb9f728bfeef13f85604c6e393e1f9c056d3
enka1504 <ramos_rafe@hotmail.com>
2023-08-07 13:43:43 -0600

    corrigiendo url

--- apps/page/api/routers.py
+++ apps/page/api/routers.py
@@ -1,7 +1,11 @@
 from rest_framework import routers
+from django.urls import path
 from apps.page.api.viewsets.general_viewset import BannerViewSet
 
 router = routers.DefaultRouter()
 router.register(r'banners', BannerViewSet)
 
-urlpatterns = router.urls
+#urlpatterns = router.urls
+urlpatterns = [
+    path('api/banners/', BannerViewSet.as_view(), name='banner-list'),
+]
