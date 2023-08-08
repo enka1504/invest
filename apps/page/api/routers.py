6a8f9ba398b7deecdc6136a632567d7ab9102b4d
enka1504 <ramos_rafe@hotmail.com>
2023-08-08 09:26:06 -0600

    url para landing-page y viewset

--- apps/page/api/routers.py
+++ apps/page/api/routers.py
@@ -1,6 +1,7 @@
 from rest_framework import routers
 from django.urls import path
 from apps.page.api.viewsets.general_viewset import BannerViewSet
+from apps.page.api.viewsets.page_viewset import PageViewSet
 
 router = routers.DefaultRouter()
 router.register(r'banners', BannerViewSet)
@@ -8,4 +9,5 @@ router.register(r'banners', BannerViewSet)
 #urlpatterns = router.urls
 urlpatterns = [
     path('api/banners/', BannerViewSet.as_view(), name='banner-list'),
+    path('api/page/', PageViewSet.as_view(), name='page-list'),
 ]
--- apps/page/api/viewsets/page_viewset.py
+++ apps/page/api/viewsets/page_viewset.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -6,77 +7,6 @@ from apps.page.models import Banner, Landing_Page
 from apps.page.api.serializers.general_serializer import BannerSerializer, LandingSerializer
 
 
-class IndexViewSet(APIView):
-
-    def get(self, request):
-        banners = Banner.objects.all()
-        landing_pages = Landing_Page.objects.all()
-
-        banner_serializer = BannerSerializer(banners, many=True)
-        landing_page_serializer = LandingSerializer(landing_pages, many=True)
-
-        data = {
-            'banners': banner_serializer.data,
-            'landing_pages': landing_page_serializer.data
-        }
-
-        return Response(data, status=status.HTTP_200_OK)
-
-# from rest_framework.response import Response
-# from rest_framework.views import APIView
-# from rest_framework.renderers import TemplateHTMLRenderer
-# from rest_framework import status
-
-# from apps.page.models import Banner, Landing_Page
-# from apps.page.api.serializers.general_serializer import BannerSerializer, LandingSerializer
-
-# class IndexViewSet(APIView):
-#     renderer_classes = [TemplateHTMLRenderer]
-#     template_name = 'page/index.html'
-
-#     def get(self, request):
-#         banners = Banner.objects.all()
-#         landing_pages = Landing_Page.objects.all()
-
-#         banner_serializer = BannerSerializer(banners, many=True)
-#         landing_page_serializer = LandingSerializer(landing_pages, many=True)
-
-#         data = {
-#             'banners': banner_serializer.data,
-#             'landing_pages': landing_page_serializer.data
-#         }
-
-#         return Response({'data': data}, status=status.HTTP_200_OK)
-# from django.views.generic import TemplateView
-# from rest_framework import status
-# from rest_framework.response import Response
-# from rest_framework.renderers import TemplateHTMLRenderer
-
-# from apps.page.models import Banner, Landing_Page
-# from apps.page.api.serializers.general_serializer import BannerSerializer, LandingSerializer
-
-
-# class IndexView(TemplateView):
-#     template_name = 'page/index.html'
-
-#     def get_context_data(self, **kwargs):
-#         banners = Banner.objects.all()
-#         landing_pages = Landing_Page.objects.all()
-
-#         banner_serializer = BannerSerializer(banners, many=True)
-#         landing_page_serializer = LandingSerializer(landing_pages, many=True)
-
-#         data = {
-#             'banners': banner_serializer.data,
-#             'landing_pages': landing_page_serializer.data
-#         }
-#         print(data)
-#         return {'data': data}
-
-#     def render_to_response(self, context, **response_kwargs):
-#         return self.response_class(
-#             request=self.request,
-#             template=self.get_template_names(),
-#             context=context,
-#             **response_kwargs
-#         )
+class PageViewSet(ListAPIView):
+    queryset = Landing_Page.objects.all()
+    serializer_class = LandingSerializer
